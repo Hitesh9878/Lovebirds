6c912f0c8dc296893439a297995f2eaa1d6cc8c8
Bonakurthi Hitesh <93438836+Hitesh9878@users.noreply.github.com>
2025-10-25 09:59:34 +0530

    Update SocketContext.jsx

--- frontend/src/contexts/SocketContext.jsx
+++ frontend/src/contexts/SocketContext.jsx
@@ -28,7 +28,7 @@ export const SocketProvider = ({ children }) => {
         return;
       }
 
-      const newSocket = io('http://localhost:5000', {
+      const newSocket = io('https://lovebirds-mwyz.onrender.com', {
         auth: { token },
         reconnection: true,
         reconnectionDelay: 1000,
